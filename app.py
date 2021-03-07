091e3f63c135c6d40592460ee5471dfe20658e03
tsopgniduhamel <tsopgniduhamel@gmail.com>
2021-03-07 20:44:19 +0100

    reconfiguring our app to work on localhost

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-#Initialize the flask App
+# Initialize the flask App
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-#default page of our web-app
+# default page of our web-app
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#To use the predict button in our web-app
-@app.route('/predict',methods=['POST'])
+# To use the predict button in our web-app
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -25,6 +29,7 @@ def predict():
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='CO2 Emission of the vehicle is :{}'.format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
